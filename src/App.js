297e519ac83a9586014cf76ea64bc1acbf85cbe4
mskradde <skradde.marco@gmail.com>
2020-08-18 15:24:14 +0200

    Modify package.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import List from "./components/List";
 
 function App() {
   return (
@@ -9,7 +10,11 @@ function App() {
         <Switch>
           <header className="app__header">
             <h1>Books I want to read</h1>
+            <input placeholder="Find your favourite Book" />
           </header>
+          <main>
+            <List>(Tolle Bücher)</List>
+          </main>
           <Route path="/books">Books</Route>
           <Route path="/books:name">Details</Route>
         </Switch>
